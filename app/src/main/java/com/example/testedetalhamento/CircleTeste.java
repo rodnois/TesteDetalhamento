4282d9b53bc54ee9993cf0c68698fac06ec0303b
rodnois <alves.rodolfo.duarte@hotmail.com>
2019-12-18 00:10:24 -0300

    comitando

---
+++ app/src/main/java/com/example/testedetalhamento/CircleTeste.java
@@ -0,0 +1,14 @@
+package com.example.testedetalhamento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CircleTeste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_teste);
+    }
+}
